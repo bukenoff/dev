4de3a97bfd7bf99c99af3224326077fcf27964d1
serik <bukenoff.s@gmail.com>
2023-05-17 18:49:44 +0700

    Fix eslint

--- nvim/lua/bukenoff/plugins/lsp/null-ls.lua
+++ nvim/lua/bukenoff/plugins/lsp/null-ls.lua
@@ -1,34 +1,34 @@
-local setup, null_ls = pcall(require, "null_ls")
+local setup, null_ls = pcall(require, "null-ls")
 if not setup then
   return
 end
 
-local formatting = null_ls.buildins.formatting
-local diagnostics = null_ls.buildins.diagnostics
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup({
+null_ls.setup {
   sources = {
     formatting.prettier,
-    formatting.stylelua,
+    formatting.stylua,
     diagnostics.eslint_d,
   },
   on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    if current_client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          vim.lsp.buf.format({
+          vim.lsp.buf.format {
             filter = function(client)
               return client.name == "null-ls"
             end,
             bufnr = bufnr,
-          })
+          }
         end,
       })
     end
   end,
-})
+}
