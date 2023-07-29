cb1b471c8e8102895938973b5a4e0d4306071ae1
serik <bukenoff.s@gmail.com>
2023-07-29 14:20:49 +0700

    :fire: Remove lspsaga

--- nvim/lua/bukenoff/plugins-setup.lua
+++ nvim/lua/bukenoff/plugins-setup.lua
@@ -51,7 +51,6 @@ return packer.startup(function(use)
   -- configure lsp servers
   use "neovim/nvim-lspconfig"
   use "hrsh7th/cmp-nvim-lsp"
-  use { "glepnir/lspsaga.nvim", branch = "main" }
   use "jose-elias-alvarez/typescript.nvim"
   use "onsails/lspkind.nvim"
   -- format and lint
