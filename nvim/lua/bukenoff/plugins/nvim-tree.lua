f0ad235b26086524043d7bdcc2641ab75b60d336
serik <bukenoff.s@gmail.com>
2023-11-22 20:27:22 +0700

    :fire: Remove custom glyphs for arrows

--- nvim/lua/bukenoff/plugins/nvim-tree.lua
+++ nvim/lua/bukenoff/plugins/nvim-tree.lua
@@ -6,19 +6,9 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+vim.cmd [[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]]
 
-nvimtree.setup({
-  renderer = {
-    icons = {
-      glyphs = {
-        folder = {
-          arrow_closed = "", -- arrow when folder is closed
-          arrow_open = "", -- arrow when folder is open
-        },
-      },
-    },
-  },
+nvimtree.setup {
   actions = {
     open_file = {
       window_picker = {
@@ -26,4 +16,4 @@ nvimtree.setup({
       },
     },
   },
-})
+}
