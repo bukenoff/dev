010bcd52262085763daf1b549d1821ae90b9bbb6
serik <bukenoff.s@gmail.com>
2023-12-30 14:13:28 +0700

    :truck: Move whichkey

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,20 +1,5 @@
 return {
-  "nvim-lua/plenary.nvim",
   "christoomey/vim-tmux-navigator",
   "vim-scripts/ReplaceWithRegister",
-  -- time tracking
   "wakatime/vim-wakatime",
-  -- which key
-  {
-    "folke/which-key.nvim",
-    config = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup {
-        -- your configuration comes here
-        -- or leave it empty to  the default settings
-        -- refer to the configuration section below
-      }
-    end,
-  },
 }
