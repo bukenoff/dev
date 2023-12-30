4eff95e66ddba1c5c8485a107f797ecddcf63dbf
serik <bukenoff.s@gmail.com>
2023-12-30 12:55:27 +0700

    :bug: Fix lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,11 +2,11 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    local base16_theme = require "lualine.themes.base16"
     require("lualine").setup({
       options = {
-        theme = base16_theme,
-      },
+        theme = base16_theme
+      }
     })
   end,
 }
-
