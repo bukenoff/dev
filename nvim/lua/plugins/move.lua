dc050a54a0c80829a7b40310161418047ff0adcd
serik <bukenoff.s@gmail.com>
2024-05-16 10:35:30 +0500

    update dependencies, fix move.nvim

--- nvim/lua/plugins/move.lua
+++ nvim/lua/plugins/move.lua
@@ -15,5 +15,22 @@ return {
     vim.keymap.set("v", "<A-k>", ":MoveBlock(-1)<CR>", opts)
     vim.keymap.set("v", "<A-h>", ":MoveHBlock(-1)<CR>", opts)
     vim.keymap.set("v", "<A-l>", ":MoveHBlock(1)<CR>", opts)
+
+    require("move").setup {
+      line = {
+        enable = true,
+        indent = true,
+      },
+      block = {
+        enable = true,
+        indent = true,
+      },
+      word = {
+        enable = true,
+      },
+      char = {
+        enable = true,
+      },
+    }
   end,
 }
