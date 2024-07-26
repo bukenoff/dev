1bd4144570d111826dab733f342ec625b29ad1d2
serik <bukenoff.s@gmail.com>
2024-07-26 13:58:26 +0500

    Ignore node_modules

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,15 +2,15 @@ return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    local nvimtree = require("nvim-tree")
+    local nvimtree = require "nvim-tree"
 
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
-    vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
-    vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
+    vim.cmd [[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]]
+    vim.cmd [[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]]
 
-    nvimtree.setup({
+    nvimtree.setup {
       view = {
         width = 35,
         relativenumber = true,
@@ -22,13 +22,18 @@ return {
           },
         },
       },
+      filesystem_watchers = {
+        ignore_dirs = {
+          "node_modules",
+        },
+      },
       filters = {
         custom = { ".DS_Store" },
       },
       git = {
         ignore = false,
       },
-    })
+    }
 
     local keymap = vim.keymap
 
