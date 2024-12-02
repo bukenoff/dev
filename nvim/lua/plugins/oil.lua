7c74bd5422b78c3c165d832beb644e20823eef52
serik <bukenoff.s@gmail.com>
2024-12-02 15:45:22 +0700

    Show hidden files

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -5,7 +5,11 @@ return {
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("oil").setup()
+    require("oil").setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
 
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end,
