b06f959079ed48e4739fb98ba999d0e2c9607d37
serik <bukenoff.s@gmail.com>
2025-04-11 14:25:31 +0700

    Add prompt to backup configs

--- source-dot-files.sh
+++ source-dot-files.sh
@@ -3,16 +3,29 @@
 current_path=pwd
 cd ~/.config
 
-# Back up your current configs, just in case
-timestamp=$(date +%F_%T)
-backup_folder_name="backup-${timestamp}"
+read -p "Back up your configs? (Y/N): " confirm
 
-mkdir backup_folder_name 
-test -d "gitui" && mv $backup_folder_name || echo "No gutui config found"
-test -d "wezterm" && mv $backup_folder_name || echo "No wezterm config found"
-test -d "tmux" && mv $backup_folder_name || echo "No tmux config found"
-test -d "nvim" && mv $backup_folder_name || echo "No nvim config found"
-test -d "kitty" && mv $backup_folder_name || echo "No kitty config found"
+if [[ "$confirm" =~ ^[yY](es)?$ ]]; then
+  timestamp=$(date +%F_%T)
+  backup_folder_name="backup-${timestamp}"
+  mkdir backup_folder_name 
+
+  echo "Backing up existing configs"
+
+  test -d "gitui" && mv $backup_folder_name || echo "No gutui config found"
+  test -d "wezterm" && mv $backup_folder_name || echo "No wezterm config found"
+  test -d "tmux" && mv $backup_folder_name || echo "No tmux config found"
+  test -d "nvim" && mv $backup_folder_name || echo "No nvim config found"
+  test -d "kitty" && mv $backup_folder_name || echo "No kitty config found"
+else
+  echo "Deleting existing configs"
+
+  test -d "gitui" && rm -rf "gitui" || echo "No gutui config found"
+  test -d "wezterm" && rm -rf "wezterm" || echo "No wezterm config found"
+  test -d "tmux" && rm -rf "tmux" || echo "No tmux config found"
+  test -d "nvim" && rm -rf "nvim" || echo "No nvim config found"
+  test -d "kitty" && rm -rf "kitty" || echo "No kitty config found"
+fi
 
 # Symlink configs to ~/.config folder
 ln -s "${current_path}/gitui" gitui
