cb77f0315244133c2f0876b4133014a2caa5022a
serik <bukenoff.s@gmail.com>
2026-01-12 08:10:36 +0500

    Fix fine-cmdline and git-worktree plugins setup

--- nvim/lua/plugins/fine-cmdline.lua
+++ nvim/lua/plugins/fine-cmdline.lua
@@ -1,47 +1,18 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim"
-  },
+  "VonHeikemen/fine-cmdline.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  lazy = false,
   config = function()
-    local null_ls = require "null-ls"
+    local fine_cmdline = require "fine-cmdline"
 
-    local formatting = null_ls.builtins.formatting
-    local diagnostics = null_ls.builtins.diagnostics
-    local code_actions = null_ls.builtins.code_actions
-    local cpp_formatting = null_ls.builtins.formatting.clang_format
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    null_ls.setup {
-      sources = {
-        require("none-ls.code_actions.eslint"),
-        formatting.prettier.with {
-          disabled_filetypes = { "json", "yaml" },
-        },
-        formatting.stylua,
-        diagnostics.eslint_d,
-        code_actions.eslint_d,
-        code_actions.gitsigns,
-        cpp_formatting,
+    fine_cmdline.setup {
+      cmdline = {
+        enable_keymaps = true,
       },
-      on_attach = function(current_client, bufnr)
-        if current_client.supports_method "textDocument/formatting" then
-          vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format {
-                filter = function(client)
-                  return client.name == "none-ls"
-                end,
-                bufnr = bufnr,
-              }
-            end,
-          })
-        end
-      end,
     }
+
+    vim.keymap.set("n", ":", function()
+      fine_cmdline.open()
+    end, { noremap = true })
   end,
 }
